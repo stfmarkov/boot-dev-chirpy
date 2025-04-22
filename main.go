c32561aab2f2342672fc0d6b4a46b48ea956d326
stfmarkov <stf.markov@gmail.com>
2025-04-22 21:21:04 +0300

    db connection and create user endpoint

--- main.go
+++ main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"strings"
 	"sync/atomic"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/stfmarkov/boot-dev-chirpy/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries      *database.Queries
+}
+
+type respValue struct {
+	CleanedBody string `json:"cleaned_body"`
+	Error       string `json:"error"`
+}
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -19,13 +40,42 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	})
 }
 
+func respondWithError(w http.ResponseWriter, status int, errString string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	resp := respValue{
+		Error: errString,
+	}
+
+	dat, err := json.Marshal(resp)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	w.Write(dat)
+}
+
 func main() {
+	godotenv.Load()
+
+	dbURL := os.Getenv("DB_URL")
+
+	db, err := sql.Open("postgres", dbURL)
+
+	if err != nil {
+		fmt.Println("Error connecting to database:", err)
+		return
+	}
+
+	dbQueries := database.New(db)
+
 	mux := http.NewServeMux()
 	var srv http.Server
 	srv.Addr = ":8088"
 	srv.Handler = mux
 
 	config := apiConfig{}
+	config.dbQueries = dbQueries
 
 	mux.Handle("/app/", config.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 
@@ -60,26 +110,6 @@ func main() {
 	churpValidator := func(w http.ResponseWriter, r *http.Request) {
 		type params struct {
 			Body string `json:"body"`
-		}
-
-		type respValue struct {
-			CleanedBody string `json:"cleaned_body"`
-			Error       string `json:"error"`
-		}
-
-		respondWithError := func(w http.ResponseWriter, status int, errString string) {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(status)
-			resp := respValue{
-				Error: errString,
-			}
-
-			dat, err := json.Marshal(resp)
-			if err != nil {
-				w.WriteHeader(500)
-				return
-			}
-			w.Write(dat)
 		}
 
 		respondWithBody := func(w http.ResponseWriter, body string) {
@@ -135,6 +165,43 @@ func main() {
 	}
 
 	mux.HandleFunc("POST /api/validate_chirp", churpValidator)
+
+	mux.HandleFunc("POST /api/users", func(w http.ResponseWriter, r *http.Request) {
+		type params struct {
+			Email string `json:"email"`
+		}
+
+		decoder := json.NewDecoder(r.Body)
+
+		parameters := params{}
+
+		err := decoder.Decode(&parameters)
+
+		if err != nil {
+			respondWithError(w, 500, "Invalid JSON")
+			return
+		}
+
+		resp, err := config.dbQueries.CreateUser(r.Context(), parameters.Email)
+
+		if err != nil {
+
+			fmt.Println("Error creating user:", err)
+
+			respondWithError(w, 500, "Error creating user")
+			return
+		}
+
+		dat, err := json.Marshal(resp)
+		if err != nil {
+			respondWithError(w, 500, "Error marshalling response")
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(201)
+		w.Write(dat)
+	})
 
 	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
 		fmt.Println("Error starting server:", err)
---
+++ sql/schema/001_users.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+CREATE TABLE users(
+    id UUID PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+-- +goose Down
+DROP TABLE users;
